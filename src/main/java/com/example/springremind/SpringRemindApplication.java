38cf6754357b6bc88c7200559cdc289a3fd9ddc2
박상일 <sangil6372@naver.com>
2025-02-18 23:06:22 +0900

    first commit

---
+++ src/main/java/com/example/springremind/SpringRemindApplication.java
@@ -0,0 +1,13 @@
+package com.example.springremind;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringRemindApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringRemindApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/springremind/SpringRemindApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.springremind;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringRemindApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
